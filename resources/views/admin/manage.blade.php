c82fffd7c00586500381bc281826f5721bf3905a
Dosmukhanbet <dosmukhanbet@gmail.com>
2017-06-29 07:55:43 +0500

    admin section bug fixed

--- resources/views/admin/manage.blade.php
+++ resources/views/admin/manage.blade.php
@@ -1,6 +1,27 @@
    @extends('admin.layout')
 
    @section('content')
-			
+			<div class="columns">
+				<div class="column is-4">
+					@include('admin.forms.addcity')
+				</div>
+				<div class="column is-4">
+					@include('admin.forms.addgroup')
+				</div>
+				<div class="column is-4">
+					@include('admin.forms.add_district')
+				</div>
+			</div>
+			<div class="columns">
+				<div class="column is-4">
+					@include('admin.forms.addshopcategory')
+				</div>
+				<div class="column is-4">
+					@include('admin.forms.addproductcategory')
+				</div>
+				<div class="column is-4">
+					@include('admin.forms.addunit')
+				</div>
+			</div>
 
    @endsection
