6e82b08fb6f9af8b76e57d75de667819ce178d2f
Dosmukhanbet <dosmukhanbet@gmail.com>
2017-07-11 19:53:57 +0500

    -mm

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,13 +9,12 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-
 Vue.component('cart', require('./components/Cart.vue'));
 Vue.component('product', require('./components/Product.vue'));
 Vue.component('flash', require('./components/Flash.vue'));
 Vue.component('update-product', require('./components/shopadmin/UpdateProduct.vue'));
 Vue.component('activate-product', require('./components/shopadmin/ActivateProduct.vue'));
-Vue.component('partnership-request', require('./components/PartnershipRequest.vue'));
+// Vue.component('partnership-request', require('./components/PartnershipRequest.vue'));
 
 const app = new Vue({
     el: '#app'
