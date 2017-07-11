ab88f2ae85b3f87b1a89e512e27558b283835c4a
Dosmukhanbet <dosmukhanbet@gmail.com>
2017-07-11 23:31:20 +0500

    css file changed

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,5 +4,4 @@ const { mix } = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
-   .options({processCssUrls: false
-});
+   .options({processCssUrls: false});
