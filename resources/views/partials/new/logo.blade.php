64317218bd091b5668340ebd84f5f4f5fd09eb59
Dosmukhanbet <dosmukhanbet@gmail.com>
2017-08-16 22:43:36 +0500

    logo svg change color

--- resources/views/partials/new/logo.blade.php
+++ resources/views/partials/new/logo.blade.php
@@ -1,5 +1,5 @@
     <a href="/" class="navbar-item">
-    		<img src="/app/lll2532.svg" alt="" width="260" height="26">
+    		<img src="/app/logo.svg" alt="" width="260" height="26">
     </a>
  
 
